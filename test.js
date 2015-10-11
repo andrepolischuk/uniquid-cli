07a8670c207583466aa99eef0f95f1e8b57bb4fb
andrepolischuk <andre.polischuk@gmail.com>
2015-10-11 16:46:51 +0200

    Use `exec` instead of `execFile`

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 'use strict';
 var test = require('ava');
-var execFile = require('exec-file');
+var exec = require('child_process').exec;
 
 test('should return version', function (t) {
   t.plan(2);
 
-  execFile('cli.js', ['--version'], {cwd: __dirname}, function (err, stdout) {
+  exec('node cli.js --version', {cwd: __dirname}, function (err, stdout) {
     t.ifError(err);
     t.true(stdout.trim().length > 0);
   });
@@ -14,7 +14,7 @@ test('should return version', function (t) {
 test('should return value', function (t) {
   t.plan(2);
 
-  execFile('cli.js', {cwd: __dirname}, function (err, stdout) {
+  exec('node cli.js', {cwd: __dirname}, function (err, stdout) {
     t.ifError(err);
     t.true(stdout.trim().length > 0);
   });
